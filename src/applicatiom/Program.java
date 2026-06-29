f14f82b58f982bd87e56e02c34be4db1d542183a
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-06-29 20:00:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/applicatiom/Program.java
+++ src/applicatiom/Program.java
@@ -1,5 +1,7 @@
 package applicatiom;
 
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
         Department obj = new Department(1 ,"Books");
 
         Seller seller =  new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
