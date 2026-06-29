05f00feec92144c2e7637d0e16cf16532332699a
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-06-29 16:15:16 -0300

    Department entity class

---
+++ src/applicatiom/Program.java
@@ -0,0 +1,10 @@
+package applicatiom;
+
+import entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1 ,"Books");
+        System.out.println(obj);
+    }
+}
